34389004b92b9d6c1398b16dc8e9e945dde4d298
hknbykl <86858721+hknbykl@users.noreply.github.com>
2021-07-03 10:52:35 +0300

    updated to test
    updated via github.com

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace test
         {
             Console.WriteLine("Test Project");
             Console.WriteLine("Made some changes here");
+            Console.WriteLine("this time updated via github.com");
+            
         }
     }
 }
